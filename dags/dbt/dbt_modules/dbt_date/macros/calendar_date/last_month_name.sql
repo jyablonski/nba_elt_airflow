99ca618393fd3c213116ec03dda34e3e2c242560
jyablonski9 <jyablonski9@gmail.com>
2021-11-10 12:35:37 -0600

    remove this gahbage

--- dags/dbt/dbt_modules/dbt_date/macros/calendar_date/last_month_name.sql
+++
@@ -1,3 +0,0 @@
-{%- macro last_month_name(short=True, tz=None) -%}
-{{ dbt_date.month_name(dbt_date.lastnext_month(1, tz), short=short) }}
-{%- endmacro -%}
--- dags/dbt/dbt_modules/dbt_date/macros/calendar_date/periods_since.sql
+++
@@ -1,3 +0,0 @@
-{%- macro periods_since(date_col, period_name='day', tz=None) -%}
-{{ dbt_utils.datediff(date_col, dbt_date.today(tz), period_name) }}
-{%- endmacro -%}
