fa4e80142b24792e176eafb09f6ff987bc00fd23
jyablonski9 <jyablonski9@gmail.com>
2021-11-17 15:09:04 -0600

    adding new dag functionality

---
+++ dags/nba_elt_pipeline_dag_qa.py
@@ -0,0 +1,114 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.email import EmailOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.amazon.aws.operators.ecs import ECSOperator
+
+JACOBS_DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['jyablonski9@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+DBT_PROFILE_DIR = '~/.dbt/'
+DBT_PROJECT_DIR = '~/airflow/dags/dbt/'
+
+def jacobs_dummy_task(dag: DAG, task_id) -> DummyOperator:
+    task_id = "dummy_task_qa" + str(task_id)
+    return DummyOperator(task_id = task_id,
+                         dag = dag)
+
+def jacobs_ecs_task(dag: DAG) -> ECSOperator:
+    return ECSOperator(
+      task_id="jacobs_airflow_ecs_task_qa",
+      dag = dag,
+      aws_conn_id="aws_ecs",
+      cluster="jacobs_fargate_cluster",
+      task_definition="jacobs_task_airflow",
+      launch_type="FARGATE",
+      overrides={},
+      network_configuration={
+          "awsvpcConfiguration": {
+              "securityGroups": ["sg-0e3e9289166404b84"],
+              "subnets": ["subnet-0652b6b91d94ebcd0", "subnet-0047afa4a7e93ec89"],
+              "assignPublicIp": "ENABLED"
+          }
+      },
+      awslogs_group="aws_ecs_logs"
+    ) 
+
+def jacobs_dbt_task1(dag: DAG) -> BashOperator:
+    task_id = "dbt_seed_qa"
+
+    return BashOperator(
+      task_id=task_id,
+      dag = dag,
+      bash_command=f"dbt seed --profiles-dir {DBT_PROFILE_DIR} --project-dir {DBT_PROJECT_DIR}"
+    )
+
+def jacobs_dbt_task2(dag: DAG) -> BashOperator:
+    task_id = "dbt_run_qa"
+    
+    return BashOperator(
+      task_id=task_id,
+      dag = dag,
+      bash_command=f"dbt run --profiles-dir {DBT_PROFILE_DIR} --project-dir {DBT_PROJECT_DIR}"
+    )
+
+def jacobs_dbt_task3(dag: DAG) -> BashOperator:
+    task_id = "dbt_test_qa"
+    
+    return BashOperator(
+      task_id=task_id,
+      dag = dag,
+      bash_command=f"dbt test --profiles-dir {DBT_PROFILE_DIR} --project-dir {DBT_PROJECT_DIR}"
+    )
+
+
+def jacobs_email_task(dag: DAG) -> EmailOperator:
+    task_id = "send_email_notification_qa"
+
+    return EmailOperator(
+      task_id=task_id,
+      dag = dag,
+      to="jyablonski9@gmail.com",
+      subject="Airflow NBA ELT Pipeline DAG Run",
+      html_content="<h3>Process Completed</h3>"
+    ) 
+
+def create_dag() -> DAG:
+    """
+    xxx
+    """
+    schedule_interval="0 11 * * *"
+    
+    dag = DAG(
+        "nba_elt_pipeline_dag_qa",
+        catchup=False,
+        default_args = JACOBS_DEFAULT_ARGS,
+        schedule_interval=schedule_interval,
+        start_date=datetime(2021, 11, 15)
+    )
+    t1 = jacobs_dummy_task(dag, 1)
+    t2 = jacobs_ecs_task(dag)
+    t3 = jacobs_dummy_task(dag, 2)
+    t4 = jacobs_dummy_task(dag, 3)
+    t5 = jacobs_dummy_task(dag, 4)
+    t6 = jacobs_dbt_task1(dag)
+    t7 = jacobs_dbt_task2(dag)
+    t8 = jacobs_dbt_task3(dag)
+    t9 = jacobs_email_task(dag)
+    t10 = jacobs_dummy_task(dag, 5)
+
+    t1 >> t2 >> [t3, t4, t5] >> t6 >> t7 >> t8 >> t9 >> t10
+
+    return dag
+
+dag = create_dag()
