893e0cdc91f3ff2bfa60c89cb2c2e6f56b1a12a8
jyablonski9 <jyablonski9@gmail.com>
2021-12-02 19:49:15 -0600

    jinja vars in email notifications

--- dags/jacobs_test_dag.py
+++ dags/jacobs_test_dag.py
@@ -8,7 +8,16 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.email import EmailOperator
 from utils import my_function, get_ssm_parameter
 
-my_function()
+JACOBS_DEFAULT_ARGS = {
+    'owner': 'jacob',
+    'depends_on_past': False,
+    'email': ['jyablonski9@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+# my_function()
 
 DBT_PROFILE_DIR = '~/.dbt/'
 DBT_PROJECT_DIR = '~/airflow/dags/dbt/'
@@ -18,26 +27,53 @@ with DAG(
     schedule_interval='0 11 * * *',
     start_date=datetime(2021, 10, 20),
     dagrun_timeout=timedelta(minutes=60),
+    default_args=JACOBS_DEFAULT_ARGS,
     catchup=False
 ) as dag:
 
     dummy_task = DummyOperator(task_id="dummy_task")
 
-    dbt_deps = BashOperator(
-      task_id="dbt_deps",
-      bash_command=f"dbt deps --profiles-dir {DBT_PROFILE_DIR} --project-dir {DBT_PROJECT_DIR}"
-    )
+    # dbt_deps = BashOperator(
+    #   task_id="dbt_deps",
+    #   bash_command=f"dbt deps --profiles-dir {DBT_PROFILE_DIR} --project-dir {DBT_PROJECT_DIR}"
+    # )
 
-    python_dummy_task = PythonOperator(
-      task_id="python_dummy_task",
-      python_callable=my_function
-    )
+    # python_dummy_task = PythonOperator(
+    #   task_id="python_dummy_task",
+    #   python_callable=my_function
+    # )
 
     send_email_notification = EmailOperator(
       task_id="send_email_notification",
       to="jyablonski9@gmail.com",
-      subject="Test Dag run",
-      html_content="<h3>Process Completed</h3>"
+      subject="Airflow Test Dag run on {{ ds }}",
+      html_content="""
+      <h3>Process {{ ts }} Completed</h3>
+      <br>
+      ds start: {{ data_interval_start }}
+      <br>
+      ds end: {{ data_interval_end }}
+      <br>
+      ds: {{ ds }}
+      <br>
+      ds nodash: {{ ds_nodash }}
+      <br>
+      ts: {{ ts }}
+      <br>
+      ts nodash: {{ts_nodash }}
+      <br>
+      dag: {{ dag }}
+      <br>
+      task: {{ task }}
+      <br>
+      run_id: {{ run_id }}
+      <br>
+      dag run: {{ dag_run }}
+      <br>
+      owner: {{ task.owner}}
+      """
     ) 
 
-    dummy_task >> [python_dummy_task, dbt_deps] >> send_email_notification
+    # dummy_task >> [python_dummy_task, dbt_deps] >> send_email_notification
+
+    dummy_task >> send_email_notification
