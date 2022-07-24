a8e7978efd7c42f9f86052f4f5aeaeaac8553771
jyablonski9 <jyablonski9@gmail.com>
2022-07-23 20:57:12 -0500

    remove this gahbage

--- dags/dbt/models/staging/staging_aws_transactions_table.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    date::TIMESTAMP AS date,
-    transaction::text
-FROM {{ source('nba_airflow', 'aws_transactions_source')}}
--- dags/sql/jacobs_practice_sql.sql
+++
@@ -1,3 +0,0 @@
-SELECT 
-    count(*) as num_records
-from jacob_db.nba_prod.prod_pbp;
