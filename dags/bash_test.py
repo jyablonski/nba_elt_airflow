e66be8688ca741c0fc45e2029c1978ad89a31f7d
jyablonski9 <jyablonski9@gmail.com>
2023-11-09 16:41:17 -0600

    z

--- dags/bash_test.py
+++ dags/bash_test.py
@@ -3,11 +3,10 @@ import os
 
 from airflow.decorators import dag, task
 from airflow.operators.bash import BashOperator
+from airflow.hooks.subprocess import SubprocessHook
 import boto3
+from include.utils import get_schedule_interval, jacobs_slack_alert, write_to_slack
 
-from include.aws_utils import check_s3_file_exists
-from include.exceptions import S3PrefixCheckFail
-from include.utils import get_schedule_interval, jacobs_slack_alert
 
 default_args = {
     "owner": "jacob",
@@ -37,10 +36,11 @@ def bash_test_pipeline():
     ):
         print(f"hi")
 
+    @task()
     def bash_task():
         sts = boto3.client("sts")
         response = sts.assume_role(
-            RoleArn="arn:aws:iam::717791819289:role/jacobs-airflow-bash-testing-role",
+            RoleArn="arn:aws:iam::717791819289:role/jacobs-airflow-bash-testing-role2",
             RoleSessionName="jacob-airflow-bash-session",
         )
 
@@ -54,15 +54,26 @@ def bash_test_pipeline():
         os.environ["EP"] = "rds.jyablonski.dev"
         os.environ["MASTERUSER"] = "myuser"
         os.environ["MYPASS"] = "mypass"
-        
 
-        return BashOperator(
-            task_id="bash_task",
-            # "scripts" folder is under "/usr/local/airflow/dags"
-            bash_command="${AIRFLOW_HOME}/include/scripts/test.sh ",
+        # return BashOperator(
+        #     task_id="bash_task",
+        #     # "scripts" folder is under "/usr/local/airflow/dags"
+        #     bash_command="${AIRFLOW_HOME}/include/scripts/test.sh ",
+        # )
+        # Specify the path to your script
+        # script_path = os.path.join(os.environ["AIRFLOW_HOME"], "include/scripts/test.sh ")ssss
+
+        # Execute the bash script using subprocess
+        print(f"hi")
+        # Specify the path to your script
+        script_path = os.path.join(
+            os.environ["AIRFLOW_HOME"], "include/scripts/test.sh"
         )
+        subprocess_hook = SubprocessHook()
+        subprocess_hook.run_command(["bash", script_path])
+        print(f"hoo")
 
-    [test_task(), bash_task()]
+    test_task() >> bash_task()
 
 
 dag = bash_test_pipeline()
