363b76705689772864f9cb10151d387024ddf759
jyablonski9 <jyablonski9@gmail.com>
2024-07-13 09:29:50 -0500

    remove that gahbage

--- tests/integration/test_pagerduty_alert.py
+++ tests/integration/test_pagerduty_alert.py
@@ -1,21 +1,18 @@
 import unittest
 from unittest.mock import patch, MagicMock
 
-# Assuming your functions are in a module named 'pagerduty_alert'
 from include.utils import jacobs_pagerduty_notification
 
 
 class TestJacobsPagerdutyNotification(unittest.TestCase):
     @patch("include.utils.send_pagerduty_notification")
     def test_jacobs_pagerduty_notification(self, mock_send_notification):
-        # Set up the mock return value
         mock_notification_instance = MagicMock()
         mock_send_notification.return_value = mock_notification_instance
         severity = "critical"
         class_type = "Test Pipeline"
         conn_id = "test_conn_id"
 
-        # Mock the context dictionary
         context = {
             "task_instance": MagicMock(
                 dag_id="test_dag", task_id="test_task", log_url="http://test_log_url"
@@ -23,7 +20,6 @@ class TestJacobsPagerdutyNotification(unittest.TestCase):
             "exception": Exception("Test Exception"),
         }
 
-        # Call the function
         notification_function = jacobs_pagerduty_notification(
             pagerduty_events_conn_id=conn_id,
             severity=severity,
@@ -31,7 +27,6 @@ class TestJacobsPagerdutyNotification(unittest.TestCase):
         )
         notification_function(context)
 
-        # Verify send_pagerduty_notification was called with correct parameters
         mock_send_notification.assert_called_once_with(
             pagerduty_events_conn_id=conn_id,
             summary="DAG test_dag Failure",
@@ -43,5 +38,4 @@ class TestJacobsPagerdutyNotification(unittest.TestCase):
             class_type=class_type,
         )
 
-        # Verify the notify method was called
         mock_notification_instance.notify.assert_called_once_with(context=context)
--- tests/integration/test_slack_alert.py
+++ tests/integration/test_slack_alert.py
@@ -1,19 +1,16 @@
 import unittest
 from unittest.mock import patch, MagicMock
 
-# Assuming your functions are in a module named 'include.utils'
 from include.utils import jacobs_slack_alert
 
 
 class TestJacobsSlackAlert(unittest.TestCase):
     @patch("include.utils.SlackWebhookOperator")
     def test_jacobs_slack_alert(self, mock_slack_operator):
-        # Set up the mock return value
         mock_slack_instance = MagicMock()
         mock_slack_operator.return_value = mock_slack_instance
         slack_conn_id = "test_slack_conn"
 
-        # Mock the context dictionary
         context = {
             "task_instance": MagicMock(
                 dag_id="test_dag",
@@ -25,11 +22,9 @@ class TestJacobsSlackAlert(unittest.TestCase):
             "execution_date": "2023-07-13T00:00:00",
         }
 
-        # Call the function
         alert_function = jacobs_slack_alert(slack_webhook_conn_id=slack_conn_id)
         alert_function(context)
 
-        # Define the expected message
         expected_message = """
                 :red_circle: Task Failed. 
             *Exception*: Test Exception
@@ -40,12 +35,10 @@ class TestJacobsSlackAlert(unittest.TestCase):
             *Log Url*: http://test_log_url 
                 """
 
-        # Verify SlackWebhookOperator was called with correct parameters
         mock_slack_operator.assert_called_once_with(
             task_id="slack_task",
             slack_webhook_conn_id=slack_conn_id,
             message=expected_message,
         )
 
-        # Verify the execute method was called
         mock_slack_instance.execute.assert_called_once_with(context=context)
